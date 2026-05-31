113b081859f40b862a371499fc4208915ff4c6a1
bhashlamoon <bara1brh@gmail.com>
2026-05-31 16:50:16 +0300

    fetch data from trivia api and fix ui issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,42 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Start from './components/Start'
 import Quiz from './components/Quiz'
 
 function App() {
-  const [isGameStarted,setIsGameStarted] = useState(false)
+  const [isGameStarted, setIsGameStarted] = useState(false)
+  const [quizData, setQuizData] = useState([])
+  useEffect(() => {
+    fetch('https://opentdb.com/api.php?amount=5&type=multiple')
+      .then(res => res.json())
+      .then(data => {
+        setQuizData(data.results || [])
+      })
+  }, [])
 
-  function startGame(){
+  function startGame() {
     setIsGameStarted(!isGameStarted)
   }
 
+  function initQuizList(quizData){
+      return quizData.map((quiz,index) => (
+     <Quiz
+      id={index}
+      key={index}
+      question={quiz.question}
+      correct_answer={quiz.correct_answer}
+      incorrect_answers={quiz.incorrect_answers} />
+      ))
+  }
 
   return (
     <>
       <main>
         {!isGameStarted && <Start handleClick={startGame} />}
-        {isGameStarted && <Quiz/>}
+        {isGameStarted && <div className="quiz-container">
+          {initQuizList(quizData)}
+          <button className='btn btn-check'>Check answers</button>
+        </div>}
       </main>
     </>
   )
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,19 +1,41 @@
-export default function Quiz() {
-    return <div className="quiz-container">
-        <div className="question">
-            <h2>How would one say goodbye in Spanish?</h2>
-            <div className="answers-container">
-                <input type="radio" name="answer" id="first"/>
-                <label htmlFor="first">first</label>
-                <input type="radio" name="answer" id="second"/>
-                <label htmlFor="second">second</label>
-                 <input type="radio" name="answer" id="third"/>
-                <label htmlFor="third">third</label>
-                  <input type="radio" name="answer" id="forth"/>
-                <label htmlFor="forth">forth</label>
-            </div>
-          <hr />
-        </div>
+import {encode,decode} from 'html-entities';
 
+export default function Quiz(props) {
+    let shuffledArray = [...props.incorrect_answers,props.correct_answer]
+    shuffledArray = shuffle(shuffledArray)
+    function shuffle(array) {
+        let currentIndex = array.length;
+        while (currentIndex != 0) {
+
+            let randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+
+            [array[currentIndex], array[randomIndex]] = [
+                array[randomIndex], array[currentIndex]];
+        }
+        return array
+    }
+
+    return <div className="question">
+        <h2>{decode(props.question)}</h2>
+        <form>
+         <fieldset id={props.id}>
+        <div className="answers-container">
+           
+            <input type="radio" name="answer" id={props.id+"first"} />
+            <label htmlFor={props.id+"first"}>{decode(shuffledArray[0])}</label>
+            <input type="radio" name="answer" id={props.id+"second"} />
+            <label htmlFor={props.id+"second"}>{decode(shuffledArray[1])}</label>
+            <input type="radio" name="answer" id={props.id+"third"} />
+            <label htmlFor={props.id+"third"}>{decode(shuffledArray[2])}</label>
+            <input type="radio" name="answer" id={props.id+"forth"} />
+            <label htmlFor={props.id+"forth"}>{decode(shuffledArray[3])}</label>
+            
+        </div>
+        </fieldset>
+        </form>
+        <hr />
     </div>
+
+
 }

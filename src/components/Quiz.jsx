4c841285fa8d9407b98191b7e5d35526b237023d
bhashlamoon <bara1brh@gmail.com>
2026-05-31 15:17:55 +0300

    quiz component structure and design

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -3,10 +3,16 @@ export default function Quiz() {
         <div className="question">
             <h2>How would one say goodbye in Spanish?</h2>
             <div className="answers-container">
-                <input type="radio" name="first" id="first"/>
-                <label htmlFor="first">radada</label>
+                <input type="radio" name="answer" id="first"/>
+                <label htmlFor="first">first</label>
+                <input type="radio" name="answer" id="second"/>
+                <label htmlFor="second">second</label>
+                 <input type="radio" name="answer" id="third"/>
+                <label htmlFor="third">third</label>
+                  <input type="radio" name="answer" id="forth"/>
+                <label htmlFor="forth">forth</label>
             </div>
-
+          <hr />
         </div>
 
     </div>

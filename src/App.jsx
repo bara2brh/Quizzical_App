1bb263674e3f59e0503c64c7f773727c30815f80
bhashlamoon <bara1brh@gmail.com>
2026-05-31 19:06:29 +0300

    fixing bugs, code refactor

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,31 @@ import { useState, useEffect } from 'react'
 import './App.css'
 import Start from './components/Start'
 import Quiz from './components/Quiz'
+import clsx from 'clsx';
+
 
 function App() {
   const [isGameStarted, setIsGameStarted] = useState(false)
+  const [isGameOver, setIsGameOver] = useState(false)
   const [quizData, setQuizData] = useState([])
   const [selectedAnswers, setSelectedAnswers] =
-    useState([{ answer: '', isCorrect: false }, { answer: '', isCorrect: false }, { answer: '', isCorrect: false }, { answer: '', isCorrect: false }, { answer: '', isCorrect: false }])
+    useState([{ answer: '', isCorrect: false }, { answer: '', isCorrect: false },
+       { answer: '', isCorrect: false }, { answer: '', isCorrect: false },
+        { answer: '', isCorrect: false }])
   const correctAnswers = quizData.map(q => q.correct_answer);
 
-  
-  useEffect(() => {
-    fetch('https://opentdb.com/api.php?amount=5&type=multiple')
-      .then(res => res.json())
-      .then(data => {
-        const results = data.results || [];
-        setQuizData(results || [])
-      })
-  }, [])
+
+ function fetchQuestions() {
+  fetch('https://opentdb.com/api.php?amount=5&type=multiple')
+    .then(res => res.json())
+    .then(data => {
+      setQuizData(data.results || []);
+    });
+}
+
+useEffect(() => {
+  fetchQuestions();
+}, []);
 
   function startGame() {
     setIsGameStarted(!isGameStarted)
@@ -33,34 +41,49 @@ function App() {
         correct_answer={quiz.correct_answer}
         incorrect_answers={quiz.incorrect_answers}
         handleChange={(e) => handleChange(e)}
+        isGameOver={isGameOver}
+        selectedAnswer={selectedAnswers[index]?.answer}
+
       />
     ))
   }
 
   function handleChange(event) {
-    const questionId = event.dataset.question_id
-    const answersArr = selectedAnswers;
-    answersArr[questionId] = {
+  const questionId = event.dataset.question_id;
+
+  setSelectedAnswers(prev => {
+    const updated = [...prev];
+
+    updated[questionId] = {
       answer: event.value,
       isCorrect: isCorrectAnswer(questionId, event.value)
     };
-    setSelectedAnswers(answersArr)
-    console.log(selectedAnswers)
-    console.log(correctAnswers)
 
-
-  }
+    return updated;
+  });
+}
 
   function isCorrectAnswer(id, answer) {
     return correctAnswers[id] == answer
   }
 
   function checkAnswers() {
-
-
-
-
+      setIsGameOver(!isGameOver)
   }
+
+  function playAgain() {
+  setIsGameOver(false);
+
+  setSelectedAnswers([
+    { answer: '', isCorrect: false },
+    { answer: '', isCorrect: false },
+    { answer: '', isCorrect: false },
+    { answer: '', isCorrect: false },
+    { answer: '', isCorrect: false }
+  ]);
+
+  fetchQuestions();
+}
 
   return (
     <>
@@ -68,7 +91,8 @@ function App() {
         {!isGameStarted && <Start handleClick={startGame} />}
         {isGameStarted && <div className="quiz-container">
           {initQuizList(quizData)}
-          <button onClick={checkAnswers} className='btn btn-check'>Check answers</button>
+          <button onClick={isGameOver? playAgain :checkAnswers} className='btn btn-check'>{isGameOver?'Play again': 'Check answers'}</button>
+          {isGameOver && <h2 className='score'>You scored {selectedAnswers.filter((answer)=>answer.isCorrect).length}/5 correct answers</h2>}
         </div>}
       </main>
     </>
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,62 +1,72 @@
-import { encode, decode } from 'html-entities';
+import { decode } from 'html-entities';
+import { useMemo } from 'react';
+import clsx from 'clsx';
 
 export default function Quiz(props) {
-
-    let shuffledArray = [...props.incorrect_answers, props.correct_answer]
-    shuffledArray = shuffle(shuffledArray)
+    const shuffledArray = useMemo(() => {
+        return shuffle([
+            ...props.incorrect_answers,
+            props.correct_answer
+        ]);
+    }, []);
 
     function shuffle(array) {
         let currentIndex = array.length;
-        while (currentIndex != 0) {
 
+        while (currentIndex !== 0) {
             let randomIndex = Math.floor(Math.random() * currentIndex);
             currentIndex--;
+
             [array[currentIndex], array[randomIndex]] = [
-                array[randomIndex], array[currentIndex]];
+                array[randomIndex],
+                array[currentIndex]
+            ];
         }
-        return array
+
+        return array;
     }
 
-    return <div className="question">
-        <h2>{decode(props.question)}</h2>
-        <form>
-            <fieldset id={props.id}>
-                <div className="answers-container">
+    return (
+        <div className="question">
+            <h2>{decode(props.question)}</h2>
 
-                    <input type="radio"
-                        data-question_id={props.id}
-                        name="answer"
-                        value={decode(shuffledArray[0])}
-                        id={props.id + "first"}
-                        onChange={(e) => props.handleChange(e.target)} />
-                    <label htmlFor={props.id + "first"}>{decode(shuffledArray[0])}</label>
-                    <input type="radio"
-                        data-question_id={props.id}
-                        name="answer"
-                        value={decode(shuffledArray[1])}
-                        id={props.id + "second"}
-                        onChange={(e) => props.handleChange(e.target)} />
-                    <label htmlFor={props.id + "second"}>{decode(shuffledArray[1])}</label>
-                    <input type="radio"
-                        data-question_id={props.id}
-                        name="answer"
-                        value={decode(shuffledArray[2])}
-                        id={props.id + "third"}
-                        onChange={(e) => props.handleChange(e.target)} />
-                    <label htmlFor={props.id + "third"}>{decode(shuffledArray[2])}</label>
-                    <input type="radio"
-                        data-question_id={props.id}
-                        name="answer"
-                        value={decode(shuffledArray[3])}
-                        id={props.id + "forth"}
-                        onChange={(e) => props.handleChange(e.target)} />
-                    <label htmlFor={props.id + "forth"}>{decode(shuffledArray[3])}</label>
+            <form>
+                <fieldset id={props.id}>
+                    <div className="answers-container">
+                        {shuffledArray.map((answer, index) => {
+                            const labelClass = clsx({
+                                "correct-answer":
+                                    props.isGameOver &&
+                                    answer === props.correct_answer,
 
-                </div>
-            </fieldset>
-        </form>
-        <hr />
-    </div>
+                                "wrong-answer":
+                                    props.isGameOver &&
+                                    answer === props.selectedAnswer &&
+                                    answer !== props.correct_answer
+                            });
+                            return <div key={index}>
+                                <input
+                                    type="radio"
+                                    data-question_id={props.id}
+                                    name={props.id}
+                                    value={decode(answer)}
+                                    id={`${props.id}-${index}`}
+                                    onChange={(e) =>
+                                        props.handleChange(e.target)
+                                    }
+                                    disabled={props.isGameOver}
+                                />
 
+                                <label className={labelClass} htmlFor={`${props.id}-${index}`}>
+                                    {decode(answer)}
+                                </label>
+                            </div>
+                        })}
+                    </div>
+                </fieldset>
+            </form>
 
+            <hr />
+        </div>
+    );
 }
